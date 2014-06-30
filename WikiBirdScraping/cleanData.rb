5184be33507b9ae18de7de96f0aa6305a5355b5b
Grant Amos <grant.amos.ext@siemens.com>
2014-06-30 19:55:58 -0400

    Script to sanitize some of the scraping artifacts (bad data, etc)

---
+++ WikiBirdScraping/cleanData.rb
@@ -0,0 +1,104 @@
+require 'json'
+require 'awesome_print'
+
+input = JSON.parse(IO.read('wikiBird.json'))
+birds = input['birds']
+birdGroups = input['birdGroups']
+
+mergeFilename = 'mergeConfig.json'
+mergeKeys = {}
+
+if File.file?(mergeFilename)
+  mergeKeys = JSON.parse(IO.read(mergeFilename))
+else
+
+  groupCollection = {}
+
+  birdGroups.each do |groupName, group|
+    order = groupCollection[group['order']]
+    if order == nil
+      order = {}
+    end
+
+    family = order[group['family']]
+    if family == nil
+      family = []
+    end
+
+    family.push(groupName)
+
+    order[group['family']] = family
+    groupCollection[group['order']] = order
+  end
+
+  ap groupCollection
+
+  groupCollection.each do |order, families|
+
+    families.each do |family, names|
+
+      winningGroupName = nil
+
+      if names.length <= 1
+        print "Short names\n"
+        next
+      end
+
+      names.each_with_index do |groupName, index|
+
+        if mergeKeys[groupName]
+          print "Key exists\n"
+          winningGroupName = mergeKeys[groupName]
+          break
+        end
+
+        print (index + 1).to_s + ": " + groupName + "\n"
+      end
+
+      if winningGroupName == nil
+        winningGroupName = names[gets.chomp().to_i - 1]
+        print "\n\n"
+      end
+
+      names.each do |group|
+        mergeKeys[group] = winningGroupName
+      end
+    end
+  end
+
+  File.open(mergeFilename, 'w') { |file|
+    file.write(JSON.pretty_generate(mergeKeys))
+  }
+
+end
+
+birdGroups.each do |birdGroupName, birdGroup|
+  birdGroup['birds'] = birdGroup['birds'].uniq
+
+  birdGroup['birds'].each do |birdName|
+    bird = birds[birdName]
+
+    if bird['scientificName'] == nil
+      birdGroups.delete(birdGroupName)
+    end
+
+    if bird['group']
+      if bird['group'] == birdGroupName
+        next
+      end
+      print birdName + " has already been assigned a group: " + bird['group'] + " -> " + birdGroupName + "\n"
+    end
+
+    bird['group'] = birdGroupName
+    bird.delete('groups')
+  end
+end
+
+File.open('wikiBird.json', 'w') { |file|
+  output = {
+    'birds' => birds,
+    'birdGroups' => birdGroups
+  }
+
+  file.write(JSON.pretty_generate(output))
+}
